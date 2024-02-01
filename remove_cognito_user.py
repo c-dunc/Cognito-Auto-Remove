c327bdf5bedeacac22c56ab6e3d2536e9e6040b5
Callum Duncan <96197413+csduncan06@users.noreply.github.com>
2024-02-01 23:04:37 +0000

    Update

--- remove_cognito_user.py
+++ remove_cognito_user.py
@@ -7,47 +7,32 @@ from botocore.exceptions import ClientError
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# TODO 
+# Fix issue with check_aged_account returning None
+# Implement check_aged_account to check if user is older than 30 days
+
+
 # Constants
 INVAL_STATUS = {"UNCONFIRMED", "RESET_REQUIRED", "FORCE_CHANGE_PASSWORD"}
 
 # Initialize boto3 client outside of functions
 client = boto3.client("cognito-idp")
 
-# User pool ID from environment variable
 user_pool_id = os.environ.get("user_pool_id")
+aged_user_threshold = os.environ.get("aged_user_threshold") # uses days e.g. 30 = 30 days
 
-def remove_user(username: str, user_pool_id: str):
-    """
-    Remove a user from the Cognito user pool.
-    
-    Args:
-        username (str): The username of the user to be removed.
-        user_pool_id (str): The ID of the Cognito user pool.
-    
-    Returns:
-        dict: A dictionary containing a success message or an error message.
-    """
+def remove_user(username: str, user_pool_id=user_pool_id):
+    logger.info(f"Removing user '{username}' from user pool '{user_pool_id}'...")
     try:
         client.admin_delete_user(UserPoolId=user_pool_id, Username=username)
         return {"message": f"Removed user {username} successfully."}
+        pass
     except ClientError as e:
-        logger.error(f"Failed to remove user {username}: {e}")
-        return {"error": f"Failed to remove user {username}: {e}"}
+        return {"error": f"Failed to remove user '{username}': {e}"}
 
-def confirm_unverified_status(user_data: str, user_pool_id: str):
-    """
-    Check if a user has an invalid status in the Cognito user pool.
-    
-    Args:
-        username (str): The username of the user to be checked.
-        user_pool_id (str): The ID of the Cognito user pool.
-    
-    Returns:
-        bool: True if the user has an invalid status, False otherwise.
-        dict: A dictionary containing an error message if the user status is not found.
-    """
+def confirm_unverified_status(user_data: dict, username: str):
+    logger.info(f"Confirming user '{username}' is in unverified status...")
     try:
-        response = client.list_users(UserPoolId=user_pool_id, Filter=f'username="{username}"')
         users = user_data.get("Users", [])
         user_status = next((user_info["UserStatus"] for user_info in users if "UserStatus" in user_info), None)
 
@@ -56,41 +41,42 @@ def confirm_unverified_status(user_data: str, user_pool_id: str):
         else:
             return False
     except ClientError as e:
-        logger.error(f"Failed to retrieve user status for {username}: {e}")
-        return {"error": f"Failed to retrieve user status for {username}: {e}"}
+        logger.error(f"Failed to retrieve user status for '{username}': {e}")
+        return {"error": f"Failed to retrieve user status for '{username}': {e}"}
 
-def check_aged_account(username: str, user_pool_id: str):
-    pass
+def check_aged_account(user_data: list, username: str):
+    logger.info(f"Checking age of user '{username}'... must be at least {aged_user_threshold} days")
+    try:
+        user_info = user_data[0]
+        creation_date = user_info.get("UserCreateDate")
+        if creation_date:
+            return str(creation_date)
+        else:
+            logger.error(f"Creation date not found for user '{username}'")
+            return None
+    except Exception as e:
+        logger.error(f"Error occurred while checking age for user '{username}': {e}")
+        return None
+
 
 def lambda_handler(event: dict, context):
-    """
-    Lambda handler function.
-    
-    Args:
-        event (dict): The event data passed to the Lambda function.
-        context (object): The runtime information passed to the Lambda function.
-    
-    Returns:
-        dict: A dictionary containing a success message or an error message.
-    """
     username = event.get("username")
     if not username:
-        logger.error("Username not provided")
         return {"error": "Username not provided"}
-    else:
-        user_data = client.list_users(UserPoolId=user_pool_id, Filter=f'username="{username}"')
 
     if not user_pool_id:
-        logger.error("User pool ID not found")
-        return {"error": f"User pool ID not found: {user_pool_id}"}
+        return {"error": "User pool ID not found"}
 
-    if confirm_unverified_status(user_data, user_pool_id):
-        logger.info(f"User {username} has invalid status. Checking age...")
-        if check_aged_account == True:
-            logger.info(f"User {username} is aged. Removing...")
-        else:
-            return {"error": f"User is invalid but not aged: {account_creation_date}/{aged_removal_interval}d"}
-        
-        return remove_user(username, user_pool_id)
-    else:
-        return {"error": f"User is valid and cannot be removed: {username}"}
+    try:
+        user_data = client.list_users(UserPoolId=user_pool_id, Filter=f'username="{username}"')
+    except ClientError as e:
+        return {"error": f"Failed to retrieve user data for '{username}': {e}"}
+
+    if not confirm_unverified_status(user_data, username):
+        return {"error": f"User is valid and cannot be removed: '{username}' - {user_data}"}
+
+    # creation_date = check_aged_account(user_data, username)
+    # if not creation_date:
+    #     return {"error": f"Creation date not found for user '{username}'"}
+
+    remove_user(username)
