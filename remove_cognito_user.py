ccd51e39cfd9f7398623776132c98beef18c8493
Callum Duncan <96197413+csduncan06@users.noreply.github.com>
2024-02-01 21:48:25 +0000

    Functions!

--- remove_cognito_user.py
+++ remove_cognito_user.py
@@ -2,22 +2,51 @@ import logging
 import boto3
 import os
 
-# Logging Init
+# GLOBAL VARIABLES
+#################################################### 
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 INVAL_STATUS = {"UNCONFIRMED", "RESET_REQUIRED", "FORCE_CHANGE_PASSWORD"}
 
-user_pool_id = os.environ.get('user_pool_id')
+user_pool_id = os.environ.get("user_pool_id") # cognito user pool id to access
+aged_removal_interval = os.environ.get("aged_removal_interval") #  how many days old the account is before removal
+
+global client
+client = boto3.client("cognito-idp")
+
+#################################################### 
 
 
-def lambda_handler(event, context):
-    """
-    Lambda handler function automatically used when Lambda is triggered.
-    :param event: username k, v passed through to determine user to remove
-    """ 
+def remove_user(username: str, user_pool_id=user_pool_id):
+    try:
+        client.admin_delete_user(UserPoolId=user_pool_id, Username=username)
+        return {"message": f"Removed user {username} successfully."}
+    except:
+        return {"error": f"Unable to remove user {username}."}
 
-    username = event.get('username')
+
+def aged_account(username: str):
+    logger.info(f"Checking if user {username} created in last 30 days.")
+
+def confirm_unverified_status(username: str):
+
+    response = client.list_users(UserPoolId=user_pool_id, Filter=f'username="{username}"')
+    users = response.get("Users", [])
+    user_status = next((user_info["UserStatus"] for user_info in users if "UserStatus" in user_info), None)
+
+    if user_status:
+        if user_status in INVAL_STATUS: 
+            return True
+        else:
+            return False
+    else: 
+        return {"message": "User status not found for user: {username}."}
+
+def lambda_handler(event: str, context):
+
+    username = event.get("username")
 
     if not username:
         logger.error("Username not provided")
@@ -27,28 +56,6 @@ def lambda_handler(event, context):
         logger.error("User pool ID not found")
         return {"error": "User pool ID not found"}
 
-    client = boto3.client("cognito-idp")
-    logger.info(f"Attempting to delete user: {username}")
-    
-    try:
-        response = client.list_users(UserPoolId=user_pool_id, Filter=f'username="{username}"')
-        users = response.get('Users', [])
-        user_status = next((user_info['UserStatus'] for user_info in users if 'UserStatus' in user_info), None)
 
-        if user_status:
-            if user_status in INVAL_STATUS: 
-                logger.info("User status is invalid. Removing...")
-                client.admin_delete_user(UserPoolId=user_pool_id, Username=username)
-                return {"message": "User removed successfully."}
-            else:
-                output = f'User status is {user_status}, WILL NOT REMOVE.'
-                logger.info(output)
-                return {"message": output}
-        else:
-            output = f'User status not found for user: {username}.'
-            logger.info(output)
-            return {"message": output}
-
-    except Exception as error:
-        logger.error(f"Error in removing user: {username}, Error: {error}")
-        return {"error": f"Error in removing user: {username}, Error: {error}"}
+    if confirm_unverified_status(username) == True:
+        logger.info(f"User {username} has invalid status. Removing user.")
